e94f73ed57f3f11a84b071b9edfc8aebc88619ac
agyeievans <evanskofiagyei06@gmail.com>
2023-08-26 16:49:19 +0000

    create product form and CSS

---
+++ src/products/forms.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict, Mapping, Optional, Type, Union
+from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+from .models import Product
+
+input_css_class = "form-control"
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'handle', 'price']
+
+    # designing form widget for all fields
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['name'].widget.attrs['placeholder'] = "Your name"
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = input_css_class
